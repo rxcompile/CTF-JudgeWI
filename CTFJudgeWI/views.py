4db41239c0012181fc7664277024de5115881d83
mrgypnocat <maestro-first@yandex.ru>
2012-10-03 16:18:35 +0400

    Update CTFJudgeWI/views.py

--- CTFJudgeWI/views.py
+++ CTFJudgeWI/views.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# файл с описанием Control'ов
 from django.shortcuts import render_to_response
 from Scoreboard.models import Flag
 
@@ -12,7 +13,7 @@ def check_flag(request):
         else:
             try:
                 Flag.objects.get(flag=q)
-                accepted = True //добавить в базу флаг к команде
+                accepted = True #добавить в базу флаг к команде
             except Flag.DoesNotExist:
                 errors.append(u'Неверный флаг.')
     return render_to_response('check_form.html', {'accepted': accepted, 'errors': errors})
