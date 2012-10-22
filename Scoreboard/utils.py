5b774031b3baa7f821f28d153cc961a1a8b7a51b
RxCompile <rxcompile@gmail.com>
2012-10-23 02:34:48 +0400

    +Extract util methods to utis.py +fix bug with double ajax send +some clean-ups

---
+++ Scoreboard/utils.py
@@ -0,0 +1,45 @@
+from ipaddr import IPAddress, IPNetwork
+from Scoreboard.models import Team, Flag, FlagLog, Task, Score, Category
+
+def addressInNetwork(ip,net):
+    user_ip = IPAddress(ip)
+    w_ip = IPNetwork(net)
+    return user_ip in w_ip
+
+def get_ip(request):
+    """Returns the IP of the request, accounting for the possibility of being
+    behind a proxy.
+    """
+    ip = request.META.get("HTTP_X_FORWARDED_FOR", None)
+    if ip:
+        # X_FORWARDED_FOR returns client1, proxy1, proxy2,...
+        ip = ip.split(", ")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR", "")
+    return ip
+
+def get_team(client_ip):
+    for t in Team.objects.all():
+        if addressInNetwork(client_ip,t.subnet):
+            return t
+    return None
+
+def isSolveTask(team, task):
+	try:
+		Score.objects.get(team=team, task=task)
+        return True
+	except:
+		return False
+
+def check_flag(team, task, sended_flag):
+    log = FlagLog.objects.create(flag=sended_flag, team=team)
+    log.save()
+    try:
+        if !isSolveTask(team,task):
+            Flag.objects.get(flag=sended_flag, task=task)
+            score = Score.objects.create(team=team, task=task)
+            score.save()
+            return True
+    except:
+        return False
+    
--- Scoreboard/views.py
+++ Scoreboard/views.py
@@ -1,69 +1,33 @@
 # coding=utf-8
 from Scoreboard.models import Team, Flag, FlagLog, Task, Score, Category
-from ipaddr import IPAddress, IPNetwork
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseNotAllowed,\
     HttpResponseNotFound
-import django.utils.simplejson as json
 from django.db.models.aggregates import Sum
+import django.utils.simplejson as json
+import Scoreboard.utils
 
-def addressInNetwork(ip,net):
-    user_ip = IPAddress(ip)
-    w_ip = IPNetwork(net)
-    return user_ip in w_ip
-
-def get_ip(request):
-    """Returns the IP of the request, accounting for the possibility of being
-    behind a proxy.
-    """
-    ip = request.META.get("HTTP_X_FORWARDED_FOR", None)
-    if ip:
-        # X_FORWARDED_FOR returns client1, proxy1, proxy2,...
-        ip = ip.split(", ")[0]
-    else:
-        ip = request.META.get("REMOTE_ADDR", "")
-    return ip
-
-def check_flag(team, task, sended_flag):
-    log = FlagLog.objects.create(flag=sended_flag, team=team)
-    log.save()
-    try:
-        try:
-            Score.objects.get(team=team, task=task)
-            return True
-        except:
-            Flag.objects.get(flag=sended_flag, task=task)
-            score = Score.objects.create(team=team, task=task)
-            score.save()
-            return True
-    except:
-        return False
-    
 def send_check_flag(request):
-    #if not request.is_ajax():
-    #    return HttpResponseNotAllowed('Ajax')
+    if not request.is_ajax():
+        return HttpResponseNotAllowed('Ajax')
     
     task_id = request.GET.get('task_id')
     team = get_team(get_ip(request))
     sended_flag = request.GET.get('flag')
     
     task = Task.objects.get(id=task_id)
-    jsonDict = { "status": check_flag(team,task,sended_flag) }
+	result = check_flag(team,task,sended_flag)
+    jsonDict = { "status": result }
     return HttpResponse( json.dumps( jsonDict ), mimetype="application/json" )
 
 def task_info(request):
     if not request.is_ajax():
         return HttpResponseNotAllowed('Ajax')
     task_id = request.GET.get('task_id')
+	team = get_team(get_ip(request))
     task = Task.objects.get(id=task_id)
-    jsonDict = {'task' : task.description, 'score' : task.score }
+    jsonDict = {'task' : task.description, 'score' : task.score, 'status' : isSolveTask(team,task) }
     return HttpResponse( json.dumps( jsonDict ), mimetype="application/json" )
-
-def get_team(client_ip):
-    for t in Team.objects.all():
-        if addressInNetwork(client_ip,t.subnet):
-            return t
-    return None
 
 def scoreboard(request):
     client_ip = get_ip(request)
